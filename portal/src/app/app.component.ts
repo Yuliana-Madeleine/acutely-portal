cf895e77d0cf8dc4b0801c62e19ded892e70fc7c
yuliana-madeleine <cc.yuliana.madeleine@outlook.com>
2019-12-09 12:57:48 -0500

    Add boostrap library to build portal page.

--- portal/src/app/app.component.ts
+++ portal/src/app/app.component.ts
@@ -7,185 +7,181 @@ import * as Highcharts from 'highcharts';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  highcharts1 = Highcharts;
-  chartOptions1 = {
-     chart: {
-        type: 'column' // Other: bar splines column
-     },
-     title: {
-        text: 'Total Revenue'
-     },
-     credits: {
-            enabled: false
-     },
-     /*subtitle: {
-        text: 'Source: WorldClimate.com"'
-     },*/
-     xAxis: {
-        // categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-     },
-     yAxis: {
-        title: {
-           text: ''
-        }
-     },
-     tooltip: {
-        valueSuffix: '$'
-     },
-     series: [
-          {
-            name: 'Location A',
-            data: [7.5]
-          },
-          {
-              name: 'Akron',
-              data: [5.0]
-          },
-          {
-              name: 'Branson',
-              data: [7.0]
-          },
-          {
-              name: 'corning',
-              data: [1.5]
-          }
-        /*{
-           name: 'Tokyo',
-           data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2,26.5, 23.3, 18.3, 13.9, 9.6]
-        },
-        {
-           name: 'New York',
-           data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8,24.1, 20.1, 14.1, 8.6, 2.5]
-        },
-        {
-           name: 'Berlin',
-           data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
-        },
-        {
-           name: 'London',
-           data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-        }*/
-     ]
-  };
-
-  highcharts2 = Highcharts;
-  chartOptions2 = {
-     chart: {
-        type: 'bar' // Other: bar splines column
-     },
-     title: {
-        text: 'Total Customers'
-     },
-     credits: {
-      enabled: false
-     },
-     xAxis: {},
-     yAxis: {
-        title: {
-           text: ''
-        }
-     },
-     tooltip: {
-        valueSuffix: 'K'
-     },
-     series: [
-          {
-            name: 'Location A',
-            data: [36]
-          },
-          {
-              name: 'Akron',
-              data: [47]
-          },
-          {
-              name: 'Branson',
-              data: [33]
-          },
-          {
-              name: 'corning',
-              data: [35]
-          }
-     ]
-  };
-
-  highcharts3 = Highcharts;
-  chartOptions3 = {
-     chart: {
-        type: 'bar' // Other: bar splines column
-     },
-     title: {
-        text: 'Total Carryout'
-     },
-     credits: {
-        enabled: false
-     },
-     xAxis: {},
-     yAxis: {
-        title: {
-           text: ''
-        }
-     },
-     tooltip: {
-        valueSuffix: 'M'
-     },
-     series: [
-          {
-            name: 'Location A',
-            data: [1.1]
-          },
-          {
-              name: 'Akron',
-              data: [0.1]
-          },
-          {
-              name: 'Branson',
-              data: [0.8]
-          },
-          {
-              name: 'corning',
-              data: [1.2]
-          }
-     ]
-  };
-
-  highcharts4 = Highcharts;
-  chartOptions4 = {
-     chart: {
-        type: 'column' // Other: bar splines column
-     },
-     title: {
-        text: 'Total Carryout'
-     },
-     credits: {
+   highcharts1 = Highcharts;
+   chartOptions1 = {
+      chart: {
+         type: 'column' // Other: bar splines column
+      },
+      title: {
+         text: 'Total Revenue'
+      },
+      credits: {
+             enabled: false
+      },
+      /*subtitle: {
+         text: 'Source: WorldClimate.com"'
+      },*/
+      xAxis: {
+         // categories: ['Location A', 'Akron', 'Branson', 'Corning']
+      },
+      yAxis: {
+         title: {
+            text: ''
+         }
+      },
+      tooltip: {
+         valueSuffix: '$'
+      },
+      series: [
+           {
+             name: 'Location A',
+             data: [7.5, 0.8, 5.7, 11.3]
+           },
+           {
+             name: 'Akron',
+             data: [7.0]
+           },
+           {
+               name: 'Branson',
+               data: [7.0]
+           },
+           {
+               name: 'Corning',
+               data: [1.5]
+           }
+         /*{
+            name: 'New York',
+            data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8,24.1, 20.1, 14.1, 8.6, 2.5]
+         },
+         {
+            name: 'Berlin',
+            data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
+         },
+         {
+            name: 'London',
+            data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+         }*/
+      ]
+   };
+ 
+   highcharts2 = Highcharts;
+   chartOptions2 = {
+      chart: {
+         type: 'bar' // Other: bar splines column
+      },
+      title: {
+         text: 'Total Customers'
+      },
+      credits: {
+       enabled: false
+      },
+      xAxis: {},
+      yAxis: {
+         title: {
+            text: ''
+         }
+      },
+      tooltip: {
+         valueSuffix: 'K'
+      },
+      series: [
+           {
+             name: 'Location A',
+             data: [36]
+           },
+           {
+               name: 'Akron',
+               data: [47]
+           },
+           {
+               name: 'Branson',
+               data: [33]
+           },
+           {
+               name: 'Corning',
+               data: [35]
+           }
+      ]
+   };
+ 
+   highcharts3 = Highcharts;
+   chartOptions3 = {
+      chart: {
+         type: 'bar' // Other: bar splines column
+      },
+      title: {
+         text: 'Total Carryout'
+      },
+      credits: {
          enabled: false
-     },
-     xAxis: {},
-     yAxis: {
-        title: {
-           text: ''
-        }
-     },
-     tooltip: {
-        valueSuffix: 'k'
-     },
-     series: [
-          {
-            name: 'Location A',
-            data: [35]
-          },
-          {
-              name: 'Akron',
-              data: [50]
-          },
-          {
-              name: 'Branson',
-              data: [30]
-          },
-          {
-              name: 'corning',
-              data: [30]
-          }
-     ]
-  };
-}
-
+      },
+      xAxis: {},
+      yAxis: {
+         title: {
+            text: ''
+         }
+      },
+      tooltip: {
+         valueSuffix: 'M'
+      },
+      series: [
+           {
+             name: 'Location A',
+             data: [1.1]
+           },
+           {
+               name: 'Akron',
+               data: [0.1]
+           },
+           {
+               name: 'Branson',
+               data: [0.8]
+           },
+           {
+               name: 'Corning',
+               data: [1.2]
+           }
+      ]
+   };
+ 
+   highcharts4 = Highcharts;
+   chartOptions4 = {
+      chart: {
+         type: 'column' // Other: bar splines column
+      },
+      title: {
+         text: 'Total Carryout'
+      },
+      credits: {
+          enabled: false
+      },
+      xAxis: {},
+      yAxis: {
+         title: {
+            text: ''
+         }
+      },
+      tooltip: {
+         valueSuffix: 'k'
+      },
+      series: [
+           {
+             name: 'Location A',
+             data: [35]
+           },
+           {
+               name: 'Akron',
+               data: [50]
+           },
+           {
+               name: 'Branson',
+               data: [30]
+           },
+           {
+               name: 'Corning',
+               data: [30]
+           }
+      ]
+   };
+ }
+ 
