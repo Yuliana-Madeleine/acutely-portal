c9be1695588bfd82affff164c5d2aff130699874
yuliana-madeleine <cc.yuliana.madeleine@outlook.com>
2019-12-19 16:57:22 -0500

    Keep static data.

--- portal/src/app/components/total-revenue/total-revenue.component.ts
+++ portal/src/app/components/total-revenue/total-revenue.component.ts
@@ -9,22 +9,31 @@ import * as Highcharts from 'highcharts';
 export class DisplayTotalRevenueComponent implements OnInit {
   highcharts = Highcharts;
   totalrevenue: any[] = [];
-  totalrevenue1 = this.totalRevenueService.getRevenue();
 
   chartOptions = {
-    chart: { type: 'column'}, // Other: bar splines column
-    title: { text: 'Total Revenue'},
-    legend: { enabled: false},
-    credits: { enabled: false},
+    chart: {
+       type: 'column' // Other: bar splines column
+    },
+    title: {
+       text: 'Total Revenue'
+    },
+    legend: {
+       enabled: false
+    },
+    credits: {
+           enabled: false
+    },
     xAxis: {
-      categories: ['Location A', 'Akron', 'Branson', 'Corning']
+      categories: ['testing']
     },
     yAxis: {
        title: {
           text: ''
        }
     },
-    tooltip: { valueSuffix: '$'},
+    tooltip: {
+       valueSuffix: '$'
+    },
     plotOptions: {
        series: {
            borderWidth: 0,
@@ -38,20 +47,39 @@ export class DisplayTotalRevenueComponent implements OnInit {
        {
            name: '',
            colorByPoint: true,
-           data: [this.totalrevenue1]
+           data: [
+            {
+               name: 'Location A',
+               y: 62.74,
+               drilldown: 'Location At'
+            },
+            {
+                  name: 'Akron',
+                  y: 10.57,
+                  drilldown: 'Akron'
+            },
+            {
+                  name: 'Branson',
+                  y: 7.23,
+                  drilldown: 'Branson'
+            },
+            {
+                  name: 'Corning',
+                  y: 5.58,
+                  drilldown: 'Corning'
+            }
+           ]
        }
    ]
  };
 
   ngOnInit() {
-    this.totalrevenue = this.totalRevenueService.getRevenue();
+    // this.totalrevenue = this.totalRevenueService.getRevenue();
     // console.log(this.totalrevenue);
   }
 
   constructor(private totalRevenueService: TotalRevenueService ) {
-    this.chartOptions.series = this.totalRevenueService.getRevenue();
-    console.log(this.chartOptions.series);
+    // this.chartOptions.series = this.totalRevenueService.getRevenue();
+    // console.log(this.chartOptions.series);
    }
-
-
 }
